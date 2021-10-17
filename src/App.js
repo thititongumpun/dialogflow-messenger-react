ae794ca41c9c7954c3ad25b4dcc1f0d0d88c00f9
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-10-18 00:21:42 +0700

    set name

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
+import { 
+Header,
+AppTitle,
+Footer,
+Name,
+Rotate } from './styled';
 
 function App() {
   useEffect(() => {
     const script = document.createElement('script');
-  
     script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
     script.async = true;
-  
     document.body.appendChild(script);
-  
     return () => {
       document.body.removeChild(script);
     }
   }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <df-messenger
-          intent="WELCOME"
-          chat-title="บอทร้านข้าวต้ม"
-          agent-id="015cb695-64c9-4c09-ae05-61d2c50519a1"
-          language-code="th"
-        ></df-messenger>
-      </header>
+      <Header>
+        <AppTitle>Chatbot ร้านข้าวต้ม</AppTitle>
+      </Header>
+        <header className="App-header">
+          <Rotate>{logo}</Rotate>
+          <df-messenger
+            intent="WELCOME"
+            chat-title="บอทร้านข้าวต้ม"
+            agent-id="015cb695-64c9-4c09-ae05-61d2c50519a1"
+            language-code="th"
+          ></df-messenger>
+        </header>
+      <Footer>
+        <Name>Thiti Tongumpun 6307011858209</Name>  
+      </Footer>
     </div>
   );
 }
---
+++ src/styled.js
@@ -0,0 +1,50 @@
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as ReactLogo } from './logo.svg';
+
+export const Header = styled.div`
+  font-size: 25px;
+  font-weight: 900;
+  line-height: 1.11;
+  color: #2b2738;
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  background-color: #282c34;
+  color: wheat;
+`;
+
+export const AppTitle = styled.h1`
+  margin-bottom: 4px;
+  margin-top: 50px;
+  width: 100%;
+`;
+
+export const Footer = styled.footer`
+  font-family: nunito-sans, sans-serif;
+  text-align: center;
+  color: wheat;
+  background-color: #282c34;
+`
+
+export const Name = styled.p`
+  margin: 0;
+  padding: 0;
+  padding-bottom: 20px;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Rotate = styled(ReactLogo)`
+  display: inline-block;
+  animation: ${rotate} 2s linear infinite;
+  padding: 2rem 1rem;
+  font-size: 1.2rem;
+`;
